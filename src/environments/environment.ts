8ab7a0c07f738411640ccd6a74e8e5066d062918
EvansMutwiri <evans.mwiti@student.moringaschool.com>
2021-08-17 00:19:09 +0300

    access fixes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: true,
-  apiUrl: 'https://api.github.com/users/EvansMutwiri?access_token=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',
+  apiUrl: 'https://api.github.com/users/EvansMutwiri?access_token=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',
   gitUrl: 'https://api.github.com/users/',
-  accessToken: '?access_token=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',
+  accessToken: '?access_token=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',
   repoAccess: '/repos'
 };
 
