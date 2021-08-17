f72146138b44d8afb7616c63de1a2c98386df58e
EvansMutwiri <evans.mwiti@student.moringaschool.com>
2021-08-17 09:14:13 +0300

    add access token

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.github.com/users/EvansMutwiri?access_token=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',
+  apiUrl: 'https://api.github.com/users/EvansMutwiri?access_token=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',
   gitUrl: 'https://api.github.com/users/',
-  accessToken: '?access_token=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',
+  accessToken: '?access_token=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',
   repoAccess: '/repos'
 };
